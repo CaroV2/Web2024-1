dc892f274b3476ee4d9ff419ccfe3b79b954925e
Carolina <carovel2608@gmail.com>
2024-02-04 21:03:15 -0500

    first layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import "./App.css"
 import { Header } from "./components/Header/Header"
 import { Body } from "./components/Body/Body";
 import { Footer } from "./components/Footer/Footer";
--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 export function Body() {
     return(
         <body>
+            <div className='banner'>
+                <img src="https://www.lavanguardia.com/files/og_thumbnail/files/fp/uploads/2018/02/13/5fa4378f1c591.r_d.505-360-0.jpeg" alt="Helsinki cathedral"/>
+            </div>
             La cuerpaza
+            <div className='call-to-action'>
+                <section className='uno'>
+                    
+                </section>
+                <img src="https://estaticos.elcolombiano.com/binrepository/848x566/52c0/780d565/none/11101/QHEV/juventus-v-villarreal-cf-round-of-sixteen-leg-two-uefa-champions-league-1536x1024_41428425_20230117105258.jpg" alt=""/>
+                <img scr="https://caracol.com.co/resizer/z3EhLSmpJv6Wp_H0qT5hOYmaJRU=/650x488/filters:format(jpg):quality(70):focal(2809x1583:2819x1593)/cloudfront-us-east-1.images.arcpublishing.com/prisaradioco/TAW7QVU3RREBVADZJPVPZZLMDE.jpg" alt=""/>
+            </div>
         </body>
     )
 }
