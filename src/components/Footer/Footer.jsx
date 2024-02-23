40cca9529a45a5819a490b2abc8d0a32ba3453d5
Carolina <carovel2608@gmail.com>
2024-02-23 01:42:15 -0500

    feat: corrected lines

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+export function Footer () {
+  return (
+    <footer>
+      <h2>Presentado por:</h2>
+      <h3>Carolina Velásquez Velasco</h3>
+      <p><em>"Donde pisa una leona no deja huella una gatita"</em></p>
+    </footer>
+  )
+}
