743885a120d37b0b67f4c5c802c0ff3aaa8e9b4e
Carolina <carovel2608@gmail.com>
2024-02-26 16:55:17 -0500

    feat: hamburger menu create

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,22 @@
-import './Header.css'
+import { useState } from 'react';
 
+import './Header.css'
 import logo from '../../assets/elinika-logo.png'
 
 export function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return(
         <header>
             <div className='logo'>
                 <img src={logo} width="50" alt='logo'/>
             </div>
-            <nav className='menu'>
+            <button onClick={() => setIsOpen(!isOpen)} className='hamburger'>
+                <div></div>
+                <div></div>
+                <div></div>
+            </button>
+            <nav className={`menu ${isOpen ? 'open' : ''}`}>
                 <a href='#'><h3>Home</h3></a>
                 <a href='#'><h3>News</h3></a>
                 <a href='#'><h3>Design</h3></a>
