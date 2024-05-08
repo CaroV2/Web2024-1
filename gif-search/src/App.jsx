58e626413b3398ff3ff004e435f481fc0893e907
Lina <limapa1220@gmail.com>
2024-05-08 17:36:51 -0500

    feat: gif structure

--- gif-search/src/App.jsx
+++ gif-search/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Search } from './components/search/Search';
+import { Gif } from './components/gif/Gif';
 
 export function App() {
   return(
       <>
       <h1>WELCOME TO GIF SEARCH</h1>
       <Search/>
+      <Gif/>
       </>
   )
 }
---
+++ gif-search/src/components/gif/Gif.jsx
@@ -0,0 +1,15 @@
+import "./Gif.css"
+
+export function Gif() {
+    return (
+        <ul>
+            <li>
+                <img
+                    src="img"
+                    alt="Title"
+                />
+                <h3>"Title"</h3>
+            </li>
+        </ul>
+    );
+}
